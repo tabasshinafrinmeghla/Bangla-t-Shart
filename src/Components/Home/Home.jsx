657b356052bcf19ba907e0a7cc83fd0cf0c38e15
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-08-08 02:03:52 +0600

    done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,47 +7,47 @@ import './Home.css';
 import toast from 'react-hot-toast';
 
 const Home = () => {
-  const tshart =useLoaderData();
+  const tshart = useLoaderData();
 
-  const [cart,setCart] =useState([]);
-  
- const handleAddToCart =tshart =>{
-  // console.log(tshart);
-const exists = cart.find(ts => ts._id === tshart._id);
-if (exists){
- toast ("you already taken this one!")
-}
-else{
-  const newCart = [...cart,tshart] 
-  setCart(newCart);
+  const [cart, setCart] = useState([]);
 
-}
+  const handleAddToCart = tshart => {
+    // console.log(tshart);
+    const exists = cart.find(ts => ts._id === tshart._id);
+    if (exists) {
+      toast("you already taken this one!")
+    }
+    else {
+      const newCart = [...cart, tshart]
+      setCart(newCart);
 
- 
-  // console.log(cart);
- }
+    }
 
- const handleRemoveFromCart = id =>{
-  // console.log(id);
-  const remaining =cart.filter(ts => ts._id !== id);
-  setCart(remaining);
- }
+
+    // console.log(cart);
+  }
+
+  const handleRemoveFromCart = id => {
+    // console.log(id);
+    const remaining = cart.filter(ts => ts._id !== id);
+    setCart(remaining);
+  }
 
 
   return (
     <div className='home-container'>
       {/* <h2>This is our Products : {tshart.length}</h2>  */}
       <div className='tshart-container '>
-      {
-        tshart.map(tshart => <Tshart key={tshart._id}
-          tshart={tshart}
-          handleAddToCart={handleAddToCart}
+        {
+          tshart.map(tshart => <Tshart key={tshart._id}
+            tshart={tshart}
+            handleAddToCart={handleAddToCart}
           ></Tshart>)
-      }
+        }
       </div>
       <div>
         <Cart cart={cart}
-        handleRemoveFromCart={handleRemoveFromCart}
+          handleRemoveFromCart={handleRemoveFromCart}
         ></Cart>
       </div>
 
--- src/Components/newProduct/newProduct.jsx
+++ src/Components/newProduct/newProduct.jsx
@@ -5,17 +5,42 @@ import Shoes from '../Shoes/Shoes';
 import "./newProduct.css"
 
 const NewProduct = () => {
-  const wedding ='collection 2023';
-  const regularUsed='nEw2023';
+  const wedding = 'collection 2023';
+  const weddingContext = createContext('Wedding purse')
+
+
+
+  const regularUsed = 'nEw2023';
   return (
     <div className='product'>
       <h2 className='newProduct'>new Product</h2>
-      <section className='flex'>
-        <Bag wedding ={wedding}></Bag>
-        <MakeupItem></MakeupItem>
-        <Shoes regularUsed={regularUsed} ></Shoes>
-      </section>
+      <weddingContext.provider value='New 2023 Wedding purse'>
+        <section className='box'>
+          <Bag wedding={wedding}></Bag>
+          <MakeupItem></MakeupItem>
+          <Shoes regularUsed={regularUsed} ></Shoes>
+        </section>
+      </weddingContext.provider>
     </div>
   );
 };
 export default NewProduct;
+
+
+
+
+/*
+1. 1st step create a context
+2.Create a provider and pass a value 
+3.Use useContext to receive the value
+
+
+
+
+
+
+*/
+
+
+
+
